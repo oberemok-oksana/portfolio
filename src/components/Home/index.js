3c7585887f2dd6ad371cdcdc1e0ce593816e618d
Oksana Oberemok <Pylypenkok@gmail.com>
2022-10-26 14:15:20 +0800

    implement Home component

---
+++ src/components/Home/index.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/o-logo.png";
+import AnimatedLetters from "../AnimatedLetters";
+import Logo from "../Logo";
+import "./index.scss";
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const nameArray = "beremok".split("");
+  const jobArray = "web developer".split("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, []);
+  });
+
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br />
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m,</span>
+          <img src={logo} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
+        </h1>
+        <h2>Frontend Developer / JavaScript/React / Youtuber</h2>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
+      </div>
+      <Logo />
+    </div>
+  );
+};
+
+export default Home;
