71a70b934cdc5497c9257823daa68a0c5893e22d
Oksana Oberemok <Pylypenkok@gmail.com>
2022-10-26 14:15:06 +0800

    implement Sidebar component

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,72 @@
+import "./index.scss";
+import Logo from "../../assets/images/o-logo.png";
+import { Link, NavLink } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import {
+  faGithub,
+  faLinkedin,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={Logo} alt="logo" />
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/oksana-oberemok-792053215/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/oberemok-oksana"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/channel/UCZNdQO5XJV9JUiQNfPsLZnQ"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faYoutube} color="4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
